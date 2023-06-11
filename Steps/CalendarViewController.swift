3da9ccf0024e229f88a2527fbd482e08ed097bb6
HarutoHamano <haruto.yhs318@gmail.com>
2023-06-11 11:05:07 +0900

    screen transition when tap a date

--- Steps/CalendarViewController.swift
+++ Steps/CalendarViewController.swift
@@ -27,57 +27,30 @@ class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDa
         
         memories = readMemories()
         
-//        if let savedDiary = realm.objects(Memory.self).filter("date == '\(self.date)'").last {
-//            print(savedDiary.steps_num)
-//        }
-        
-//        getModel()
-        
     }
     
-    
-//    func getModel() {
-//        let results = realm.objects(Memory.self)
-////        var eventModels: [[String:Any]] = []
-//        for result in results {
-//            eventModels.append(["date": result.date,
-//                                "steps_num": result.steps_num,
-//                                "distance": result.distance,
-//                                "location": result.location,
-//                                "photo": result.photo,
-//                               ])
-//        }
-//    }
-    
-//    func filterModel() {
-//        var filterdEvents: [[String:Any]] = []
-//        for eventModel in eventModels {
-//            if eventModel["date"] as! String == stringFromDate(date: selectedDate as Date, format: "yyyy/MM/dd") {
-//                filterdEvents.append(eventModel)
-//            }
-//        }
-////        filterdModels = filterdEvents
-//    }
     
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy/MM/dd"
         print(dateFormatter.string(from: date))
-//        filterModel()
-//        tableView.reloadData()
         
-        if let savedDiary = realm.objects(Memory.self).filter("date == '\(dateFormatter.string(from: date))'").last {
-            print(savedDiary.steps_num)
-        } else {
-            print("no data")
+        selectedMemory = realm.objects(Memory.self).filter("date == '\(dateFormatter.string(from: date))'").last
+        if selectedMemory != nil {
+            self.performSegue(withIdentifier: "visitMemoryView", sender: nil)
         }
     }
     
     func readMemories() -> [Memory]{
         return Array(realm.objects(Memory.self))
     }
-        
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "visitMemoryView" {
+            let MemoryViewController = segue.destination as! MemoryViewController
+            MemoryViewController.selectedMemory = self.selectedMemory!
+        }
+    }
 }
 
 
