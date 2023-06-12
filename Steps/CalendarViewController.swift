eef6d4d513a696b77038aafdaaaf13e5bcd88627
HarutoHamano <haruto.yhs318@gmail.com>
2023-06-12 19:25:34 +0900

    designed calendar

--- Steps/CalendarViewController.swift
+++ Steps/CalendarViewController.swift
@@ -15,6 +15,8 @@ class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDa
     let realm = try! Realm()
     var memories: [Memory] = []
     var selectedMemory: Memory? = nil
+    var datesWithEvents: Set<String> = []
+    var stepsWithEvents: Set<Int> = []
     
     var eventModels: [[String:Any]] = []
     @IBOutlet weak var calendar: FSCalendar!
@@ -40,6 +42,63 @@ class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDa
             self.performSegue(withIdentifier: "visitMemoryView", sender: nil)
         }
     }
+    
+    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int{
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.timeZone = TimeZone.current
+        formatter.locale = Locale.current
+        let calendarDay = formatter.string(from: date)
+
+        // Realmオブジェクトの生成
+        let realm = try! Realm()
+        // 参照（全データを取得）
+        let memories = realm.objects(Memory.self)
+
+        if memories.count > 0 {
+            for i in 0..<memories.count {
+                if i == 0 {
+                    datesWithEvents = [memories[i].date]
+                } else {
+                    datesWithEvents.insert(memories[i].date)
+                }
+            }
+        } else {
+            datesWithEvents = []
+        }
+        print(datesWithEvents)
+        return datesWithEvents.contains(calendarDay) ? 1 : 0
+    }
+    
+//    func calendar(_ calendar: FSCalendar, titleFor date: Date) -> String? {
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "yyyy/MM/dd"
+//        formatter.calendar = Calendar(identifier: .gregorian)
+//        formatter.timeZone = TimeZone.current
+//        formatter.locale = Locale.current
+//        let calendarDay = formatter.string(from: date)
+//
+//        // Realmオブジェクトの生成
+//        let realm = try! Realm()
+//        // 参照（全データを取得）
+//        let memories = realm.objects(Memory.self)
+//
+//        if memories.count > 0 {
+//            for i in 0..<memories.count {
+//                if i == 0 {
+//                    stepsWithEvents = [memories[i].steps_num]
+//                } else {
+//                    stepsWithEvents.insert(memories[i].steps_num)
+//                }
+//            }
+//        } else {
+//            stepsWithEvents = []
+//        }
+//        print(stepsWithEvents)
+//        return "\(stepsWithEvents)"
+//    }
     
     func readMemories() -> [Memory]{
         return Array(realm.objects(Memory.self))
